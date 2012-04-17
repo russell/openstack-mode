d9f7c2753fb8187472f561f8f9669aaa927577f6
Russell Sim <russell.sim@gmail.com>
2012-04-17 22:28:14 +1000

    added copyright

--- openstack-mode.el
+++ openstack-mode.el
@@ -1,3 +1,27 @@
+;;; openstack-mode.el --- an interface to openstack
+
+;; Copyright (C) 2012 Free Software Foundation, Inc.
+
+;; Author   : Russell Sim <russell.sim@gmail.com>
+;; URL      : https://github.com/russell/openstack-mode
+;; Version  : 0.1
+;; Keywords : openstack, rpc
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
 (eval-when-compile (require 'cl))
 
 (require 'osapi)
